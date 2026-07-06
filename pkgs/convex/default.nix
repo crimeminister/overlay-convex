df5dc60b5d060e1a147cef50684a3c4580c62ef4
Robert Medeiros <robert@crimeminister.org>
2026-07-06 15:18:35 -0400

    chore(convex): add maven-install-plugin dependency

--- pkgs/convex/default.nix
+++ pkgs/convex/default.nix
@@ -29,6 +29,7 @@
 
   manualMvnArtifacts = [
     "org.apache.maven.plugins:maven-install-plugin:3.1.2"
+    "org.apache.maven.plugins:maven-install-plugin:3.1.4"
     "org.apache.maven.plugins:maven-assembly-plugin:3.7.1"
     "io.javalin.community.openapi:openapi-annotation-processor:7.2.2"
   ];
