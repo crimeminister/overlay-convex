f0ae36809d71067fb32966484f37467ff170fdea
Robert Medeiros <robert@crimeminister.org>
2026-07-17 10:45:02 -0400

    chore(convex): 0.8.8 -> 0.8.9

--- default.nix
+++ default.nix
@@ -18,9 +18,9 @@ in
 {
   # Decentralized platform for the Internet of Value.
   convex = self.callPackage ./pkgs/convex {
-    version = "0.8.8";
-    hash = "sha256-y6cPEN/MPbcdmQLzmj8sdkBCuCQ2LDKyxWUV1YGE5RY=";
-    mvnHash = "sha256-uw1nlrKs0q/SqzZdXwW4KwR5hFv+Z3UX9hLqIXpTQrg=";
+    version = "0.8.9";
+    hash = "sha256-k5WeeVMJMpCdf5+j5U3jYPwXK4fdoWB7igDYFt5EOGE=";
+    mvnHash = "sha256-hakYK9fjiu233/6B/RDvydA9HPFaBkbgvX0o4KG27r4=";
   };
 
   # Tree-sitter parser for Convex Lisp.
