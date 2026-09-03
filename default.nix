6a108cc0562138edde277449523d7bb2fe36b89e
Robert Medeiros <robert@crimeminister.org>
2026-09-03 18:28:24 -0400

    chore(convex): 0.8.15 -> 0.8.16

--- default.nix
+++ default.nix
@@ -18,8 +18,8 @@ in
 {
   # Decentralized platform for the Internet of Value.
   convex = self.callPackage ./pkgs/convex {
-    version = "0.8.15";
-    hash = "sha256-eyFZvj6aqmv4GF2RN2HTVkIB5c8ZgBOO/fyOfAD8uak=";
+    version = "0.8.16";
+    hash = "sha256-wioqtIwPeAqLYpIafKImu1iTFco10CPVwZd3l81BU04=";
     mvnHash = "sha256-+9Ojvw0r+3FkLa+zNZHwytaeb6Cv5jNlInGkwHNtv9E=";
   };
 
